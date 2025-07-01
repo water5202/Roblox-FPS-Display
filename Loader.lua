88fb5493319ceea1cb47255b6d377feac164b7a2
water.5202 <yeahidkorb@gmail.com>
2025-07-01 19:48:42 +0100

    Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -16,7 +16,7 @@ local rainbowT = 0
 local lerpT = 0
 local var = false
 local espval = false
-local localplayer = Players.LocalPlayer
+local localPlayer = Players.LocalPlayer
 
 if CoreGUI:FindFirstChild("53637265656e4775690d0a") then
     StarterGui:SetCore("SendNotification", {
